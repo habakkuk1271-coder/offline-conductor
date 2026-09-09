e24109cfce005b507561cd4843301aa51b657190
habakkuk1271-coder <habakkuk1271@gmail.com>
2026-09-09 11:10:05 +0800

    Release offline conductor V2
    * Build V2 menu, mystery viewer and LINE QR screen
    
    * Add V2 offline service worker
    
    * Update V2 app manifest
    
    * Add offline LINE QR asset
    
    * Use local SVG LINE QR asset
    
    * Cache LINE QR SVG offline
    
    * Make V2 offline cache resilient

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='offline-conductor-v2-2';
+const CORE=['./','./index.html','./manifest.webmanifest','./assets/line-qr.svg'];
+self.addEventListener('install',e=>e.waitUntil((async()=>{const c=await caches.open(CACHE);await Promise.all(CORE.map(async u=>{try{await c.add(u)}catch(_){}}));await self.skipWaiting()})()));
+self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith((async()=>{const hit=await caches.match(e.request);if(hit)return hit;try{const res=await fetch(e.request);if(res&&res.ok){const c=await caches.open(CACHE);c.put(e.request,res.clone()).catch(()=>{})}return res}catch(_){if(e.request.mode==='navigate')return (await caches.match('./index.html'))||Response.error();return Response.error()}})())});
